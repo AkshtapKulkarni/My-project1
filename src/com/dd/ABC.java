0b70ebfc3d813032997e9ef2e9830b0e9c40e697
AkshtapKulkarni <kakshta1003@gmail.com>
2024-08-16 15:41:10 +0530

    second time add file akshta

---
+++ src/com/dd/ABC.java
@@ -0,0 +1,10 @@
+package com.dd;
+
+public class ABC {
+public static void main(String[] args) {
+	System.out.println("main");
+	System.out.println("hello");
+	System.out.println("Start");
+	System.out.println("HEllo skills it");
+}
+}
